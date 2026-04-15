8cd581d230f105c9aeb033e9c392660b04e3f62a
7hangtime <dthrowe1@charlotte.edu>
2026-04-14 22:45:43 -0400

    Added all pieces to support the completed query

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,7 +1,15 @@
 import * as taskService from '../services/taskService.js';
 
 export async function getTasks(req, res, next) {
-  const tasks = await taskService.getAllTasks();
+  let completed = req.query.completed
+
+  if (completed === "true") {
+    completed = true
+  } else if (completed === "false") {
+    completed = false
+  }
+  const tasks = await taskService.getAllTasks(completed);
+  console.log(req.query.completed, typeof req.query.completed); // test to see if Service is getting a string or bool
   res.json(tasks);
 }
 
